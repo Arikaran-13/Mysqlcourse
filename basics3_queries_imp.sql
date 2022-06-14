2e5716952d94c92b65497f607fcbe691aa96ddc9
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-06-14 19:02:34 +0530

    foreign key nd index

--- basics3_queries_imp.sql
+++ basics3_queries_imp.sql
@@ -49,6 +49,53 @@ alter table employee modify id int auto_increment;
 
 Describe  employee;
 
+drop table employee;
+
+-- foreign key
+
+create table branch(
+branch_id int primary key auto_increment,
+location varchar(10),
+addr varchar(40) 
+
+);
+drop table employee;
+create table employee(
+emp_id int primary key auto_increment,
+ename varchar(25) not null,
+job_desc varchar(25),
+sal int ,
+branch_id int ,
+constraint fk foreign key (branch_id) references branch(branch_id)
+
+);
+ insert into branch values
+(1,"chennai","abc road tambaram"),
+(2,"hyd","123 xyz"),
+(3,"cbm","15th floor mount road");
+
+insert into employee values(1,"aarthi","HR",250000,2),
+(2,"mohan", "sales",20000,3),
+(3,"kumar","engineer",35000,1),
+(4,"salim","analyst",230000,2),
+(5,"abinaya","HR",250000,2),
+(6,"Harini","QA",190000,1),
+(7,"vedika","Manager",350000,3),
+(8,"Arikaran","Dev",600000,3),
+(9,"ram","CEO",100000,1);
+
+select * from employee;
+select *from branch;
+
+-- index
+show index from employee;
+
+create index name_index on  employee(ename);
+show index from employee;
+
+
+
+
 
 
 
